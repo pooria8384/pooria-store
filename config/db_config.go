bcb45e3949c7373a983ca34595614a7297444237
pooria8384 <mbnh3pt@gmail.com>
2024-10-19 01:50:56 +0330

    singleton in db config

--- config/db_config.go
+++ config/db_config.go
@@ -5,73 +5,35 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-}
+var Db *sql.DB
+var once sync.Once
 
-func ConnectDefaultDB() *sql.DB {
-	LoadEnv()
+func ConnectDB() *sql.DB {
+	once.Do(func() {
+		dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=UTC",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"))
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=%s sslmode=disable TimeZone=UTC",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_PORT"))
-
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		log.Fatal("Failed to connect to default database: ", err)
-	}
-
-	return db
-}
-
-func ConnectToSeedUserDB() *sql.DB {
-
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=UTC",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"))
-
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		log.Fatal("Failed to connect to seed-user database: ", err)
-	}
-
-	if err := db.Ping(); err != nil {
-		log.Fatal("DB unreachable: ", err)
-	}
-
-	fmt.Println("Successfully connected to the seed-user database!")
-	return db
-}
-
-func CreateDatabaseIfNotExists(db *sql.DB, dbName string) error {
-	var exists bool
-	query := fmt.Sprintf("SELECT EXISTS(SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('%s'))", dbName)
-	err := db.QueryRow(query).Scan(&exists)
-	if err != nil {
-		return fmt.Errorf("error checking if database exists: %v", err)
-	}
-
-	if !exists {
-		_, err = db.Exec(fmt.Sprintf("CREATE DATABASE \"%s\"", dbName))
+		db, err := sql.Open("postgres", dsn)
 		if err != nil {
-			return fmt.Errorf("error creating database: %v", err)
+			log.Fatal("Failed to connect to database: ", err)
 		}
-		fmt.Printf("Database '%s' created successfully!\n", dbName)
-	} else {
-		fmt.Printf("Database '%s' already exists.\n", dbName)
-	}
-	return nil
+
+		if err := db.Ping(); err != nil {
+			log.Fatal("DB unreachable: ", err)
+		}
+
+		fmt.Println("Successfully connected to database!")
+		Db = db
+	})
+
+	return Db
 }
